788ae08ebad055bd6978d42fba5dd3662eb1167e
Jeffrey Yun <thisishaoyu@gmail.com>
2019-04-13 20:54:23 -0700

    Setting up server

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Assistant
+
+L o n g Dog: A seeing eye dog at long range
+
+LongDog (working title) is an app to help blind people "see". It features
+
+
--- server.py
+++ server.py
@@ -12,21 +12,24 @@ app = Flask(__name__)
 def start():
     js = json.loads(request.data.decode('utf-8'))
     print(js)
-    # Video location
-    loc = "/Users/haoyuyun/Desktop/ATT/Assistant/{}".format(js['link'])
-    print(loc)
+
     try:
-        # os.system("python3 tf-pose/run.py --model=mobilenet_v2_small --resolution=432x368 --image={}".format(loc))
-        os.system("python3 tf-pose/run_webcam.py --model=mobilenet_v2_small --resize=432x368 --camera=0")
+        mode = request.mode
+        image = request.files['image']
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+        image.save(filename)
     except:
-        pass
-    # TODO: Send POST request to IBM Watson depending on mode (given in header)
-    # TODO: use enums
+        return jsonify({"result": "Error"})
+
+    # Do something depending on mode
     if mode == "caption":
-        return captionImage()
+        return captionImage(image)
     elif mode == "faces":
-        return describeFaces()
-    return {"result": "Error"}
+        return describeFaces(image)
+    elif mode == "test":
+        return jsonity({"result": "Success!"})
+    else:
+        return jsonify({"result": "Error"})
 
 def describeFaces():
     """
@@ -34,7 +37,8 @@ def describeFaces():
     Output: JSON, with info
     e.g. {"result": "two people: one is 80% happy, two is 72% sad"}
     """
-    return {}
+    json = {}
+    return jsonify(json)
 
 
 # Compare Azure and IBM Watson captioning
@@ -45,7 +49,7 @@ def captionImage():
     e.g. {"result": "a red fox in a green field", "confidence": 88.5, "other stuff".....}
     """
     json = {}
-    return json
+    return jsonify(json)
 
 if __name__ == "__main__":
     app.run(debug=True)
